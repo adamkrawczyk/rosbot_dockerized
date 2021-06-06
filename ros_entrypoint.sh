eb5bb377c659946096e3fcca730f7dd31f543485
dominikn <dominik.nwk@gmail.com>
2021-06-06 16:15:34 +0200

    added TODO about firmware update policy in the Dockerfile

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -6,6 +6,11 @@ source /ros2_ws/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI=file:///cyclonedds.xml
 
+# TODO: 
+# Zrobić w firmwarze na STM32 serwis ROS2: "get_version", ktory zwraca wiadomosc z biezaca wersja.
+# - Jezeli wersja fv na STM32 jest taka sama, to nie przechodzimy dalej
+# - Jezeli wersja fv na STM32 jest inna niz ta w tym obrazku Dockera, albo nie ma wcale odpowiedzi, to robimy:
+
 /flash_firmware.sh
 
 exec "$@"
